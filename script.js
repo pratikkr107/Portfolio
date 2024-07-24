21241b55cb5220d0f491532a0f2aa713573c7940
pratikkr107 <146076433+pratikkr107@users.noreply.github.com>
2024-07-24 16:31:53 +0530

    files added

---
+++ script.js
@@ -0,0 +1,89 @@
+// Created By: Manisha Varshney, On: 7/28/2023 
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Photographer", "Coder", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Photographer", "Coder", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
+//Theme Color Switching
+const styleSwitchToggle = document.querySelector(".mode");
+styleSwitchToggle.addEventListener("click", () => {
+    document.querySelector(".color-switch").classList.toggle("open");
+})
+window.addEventListener("scroll", () => {
+    if( document.querySelector(".color-switch").classList.contains("open")){
+        document.querySelector(".color-switch").classList.remove("open");
+    }
+});
+const alternateStyles = document.querySelectorAll(".alternate");
+function setActiveStyle(color){
+    alternateStyles.forEach((style) => {
+        if(color===style.getAttribute("title")){
+            style.removeAttribute("disabled");
+        }
+        else{
+            style.setAttribute("disabled","true");
+        }
+    });
+}
